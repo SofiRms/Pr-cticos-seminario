947521a72dbbffc29d5d513080c1cd07b0bf3383
sofi <sofiamagaliramos@gmail.com>
2023-05-10 15:51:31 -0300

    agregué función loss

--- index.js
+++ index.js
@@ -19,7 +19,23 @@ async function Execute() {
     const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
     // Train the model using the data.
-    await model.fit(xs, ys, { epochs: epocas });
+    // await model.fit(xs, ys, { epochs: epocas });
+
+    const history = await model.fit(xs, ys, {
+        epochs: epocas,
+        callbacks: {
+          onEpochEnd: (epoch, logs) => {
+             console.log(logs);
+             console.log("/n");
+             console.log(`Epoch ${epoch+1} - Loss: ${logs.loss.toFixed(4)}`);
+          }
+        }
+      });
+
+      // Imprimir la pérdida final
+      console.log(`Final Loss: ${history.history.loss[epocas-1].toFixed(4)}`);
+     
+      alert("termino de entrenar");
 
     // Use the model to do inference on a data point the model hasn't seen.
     // Should print approximately 39.
@@ -28,7 +44,7 @@ async function Execute() {
     
 
 function predecir(ejex){
-console.log(ejex)
+
 document.getElementById('micro-out-div').innerText =
         model.predict(tf.tensor2d([ejex], [1, 1])).dataSync();
     }
