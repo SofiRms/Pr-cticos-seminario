e7174ce2c385f77fa2717b5e5b95f00b2d7d3c45
sofi <sofiamagaliramos@gmail.com>
2023-05-16 16:02:37 -0300

    agregué gráfico de pérdidas

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Create a simple model.
 const model = tf.sequential();
 
+
+
 async function Execute() {
 
     // Obtener los valores ingresados por el usuario
-    const ejex = parseInt(document.getElementById("ejex").value);
+
     const epocas = document.getElementById("epocas").value;
     console.log('Executing')
 
@@ -15,36 +17,38 @@ async function Execute() {
 
     // Generate some synthetic data for training.Function que debe predecir (y = 2x - 1)
     //la 2da dimension se interpreta: 6 posiciones  en 1 dimension
-    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-    const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+
+    const tensorX = tf.tensor2d([-2, -1, 0, 1, 2, 3, 4], [7, 1]);
+    const tensorY = tf.tensor2d([-4, -1, 2, 5, 8, 11, 14], [7, 1])
+
 
     // Train the model using the data.
-    // await model.fit(xs, ys, { epochs: epocas });
+    await model.fit(tensorX, tensorY, { epochs: epocas });
 
-    const history = await model.fit(xs, ys, {
+    const history = await model.fit(tensorX, tensorY, {
         epochs: epocas,
-        callbacks: {
-          onEpochEnd: (epoch, logs) => {
-             console.log(logs);
-             console.log("/n");
-             console.log(`Epoch ${epoch+1} - Loss: ${logs.loss.toFixed(4)}`);
-          }
-        }
-      });
+        callbacks: tfvis.show.fitCallbacks(
+            { name: 'Training Performance' },
+            ['loss', 'mse'],
+            { height: 200, callbacks: ['onEpochEnd'] }
+        )
 
-      // Imprimir la pérdida final
-      console.log(`Final Loss: ${history.history.loss[epocas-1].toFixed(4)}`);
-     
-      alert("termino de entrenar");
+    });
 
-    // Use the model to do inference on a data point the model hasn't seen.
-    // Should print approximately 39.
-    predecir(ejex)
+    // Imprimir la pérdida final
+    console.log(`Final Loss: ${history.history.loss[epocas - 1].toFixed(4)}`);
+
+    alert("termino de entrenar");
+
+
 }
-    
 
-function predecir(ejex){
+function predecir() {
+    const ejex = parseInt(document.getElementById("ejex").value);
 
-document.getElementById('micro-out-div').innerText =
+    console.log('ingresa a predecir')
+    document.getElementById('micro-out-div').innerText =
         model.predict(tf.tensor2d([ejex], [1, 1])).dataSync();
-    }
+    console.log('terminó de predecir')
+
+}
